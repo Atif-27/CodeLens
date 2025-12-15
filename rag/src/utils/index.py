ad86d259df9972e924833200b05843d6bf42ded7
Atif Ali <atif276ali@gmail.com>
2025-12-16 00:35:01 +0530

    fix: Update import paths in index.py to include utils prefix

--- rag/src/utils/index.py
+++ rag/src/utils/index.py
@@ -1,5 +1,5 @@
-from load_repo import load_github_repo_fast  
-from ingestor import chunk_and_store_in_qdrant
+from utils.load_repo import load_github_repo_fast
+from utils.ingestor import chunk_and_store_in_qdrant
 
 def loadDocAndStore(repoName, githubToken, projectId):
     docs = load_github_repo_fast(repoName, githubToken)
